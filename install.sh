33be2f4f9d735646966d90cd49e59a519dc4fcb4
Martino Visintin <mvisinti@thoughtworks.com>
2014-07-23 01:10:25 +0200

    convert Rakefile to simple bash script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# bash "strict mode" : http://redsymbol.net/articles/unofficial-bash-strict-mode
+set -euo pipefail
+IFS=$'\n\t'
+
+# copy dotfiles
+stow --target=$HOME vim zsh ruby haskell
+
+# install oh-my-zsh
+rm -rf $HOME/.bash* # yeeeha!
+git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+
+# install scm_breeze
+git clone git://github.com/ndbroadbent/scm_breeze.git $HOME/.scm_breeze
+$HOME/scm_breeze/install.sh
+
+# install vundle
+git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+# FIXME: still doesn't work on linux
+echo "\n" | vim - +PluginInstall +qall!
+

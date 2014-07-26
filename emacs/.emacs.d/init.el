86a341f90e848d46b7b6e6b297122ff09dc18689
Martino Visintin <mvisinti@thoughtworks.com>
2014-07-26 16:29:10 +0200

    begin to make it usefulgs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,26 +1,43 @@
 (require 'package)
+(setq package-list '(evil
+                     evil-leader
+
+                     better-defaults
+
+                     projectile
+
+                     rainbow-delimiters
+
+                     git-gutter
+
+                     color-theme-solarized
+
+                     markdown-mode
+
+                     clojure-mode
+                     clojure-test-mode
+                     cider))
+
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
-(defvar my-packages '(evil
-                      better-defaults
+;;; fetch the list of available packages
+(unless package-archive-contents
+  (package-refresh-contents))
 
-                      color-theme-solarized
+;;; install the missing packages
+(dolist (pkg package-list)
+        (unless (package-installed-p pkg)
+                (package-install pkg)))
 
-                      markdown-mode
-
-                      clojure-mode
-                      clojure-test-mode
-                      cider))
-
-(dolist (p my-packages)
-        (when (not (package-installed-p p))
-              (package-install p)))
+;;; projectile = project interaction library
+(require 'projectile)
+(projectile-global-mode)
 
 ;;; evil mode = vim keymappings
 (require 'evil)
-(evil-mode 1)
+(evil-mode t)
 ;;; esc quits
 (define-key evil-normal-state-map [escape] 'keyboard-quit)
 (define-key evil-visual-state-map [escape] 'keyboard-quit)
@@ -30,7 +47,22 @@
 (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 
-;;; markdown
+;;; evil leader = vim leader
+(require 'evil-leader)
+(global-evil-leader-mode)
+(evil-leader/set-leader ",")
+(evil-leader/set-key
+  "," 'projectile-find-file)
+
+;;; git-gutter = git diffs in the gutter
+(require 'git-gutter)
+(global-git-gutter-mode t)
+
+;;; rainbow-delimiters = rainbow parens
+(require 'rainbow-delimiters)
+(global-rainbow-delimiters-mode)
+
+;;; markdown-mode
 (autoload 'markdown-mode "markdown-mode"
      "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))

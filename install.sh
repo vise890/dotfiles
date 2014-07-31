4bea5e6d6dbb9648b4e9ba7a76aafd350f6411f8
Martino Visintin <mvisinti@thoughtworks.com>
2014-07-31 12:14:24 +0200

    be more talkative

--- install.sh
+++ install.sh
@@ -5,23 +5,23 @@ IFS=$'\n\t'
 
 DOTFILES_DIR=`pwd`
 
-# copy dotfiles
+echo "==> copy dotfiles"
 stow --target=$HOME vim zsh ruby haskell
 
-# install oh-my-zsh
+echo "==> install oh-my-zsh"
 rm -rf $HOME/.bash* # yeeeha!
 git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh || true
 
-# install scm_breeze
+echo "==> install scm_breeze"
 git clone https://github.com/ndbroadbent/scm_breeze.git $HOME/.scm_breeze || true
 $HOME/.scm_breeze/install.sh
 
-# install vundle
+echo "==> install vundle"
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim || true
 # FIXME: still doesn't work on linux
 echo "\n\n" | vim - +PluginInstall +qall!
 
-# compile vimproc native extensions
+echo "==> compile vimproc native extensions"
 cd $HOME/.vim/bundle/vimproc.vim
 make
 cd $DOTFILES_DIR

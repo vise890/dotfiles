6f8563e4c2b2a65aa0d6b170bd55cf778229edde
Martino Visintin <mvisinti@thoughtworks.com>
2014-08-12 19:42:27 +0200

    give lein some love (stow by default)

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 DOTFILES_DIR=`pwd`
 
 echo "==> copy dotfiles"
-stow --restow --target=$HOME vim zsh haskell
+stow --restow --target=$HOME vim zsh haskell lein
 
 echo "==> install oh-my-zsh"
 rm -rf $HOME/.bash* # yeeeha!

5adad0c00fe1fdc3a6259107d35e34cefb40dab1
Martino Visintin <mvisinti@thoughtworks.com>
2014-08-12 21:35:34 +0200

    add slamhound shortcut

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[lein-kibit "0.0.8"]
                   [jonase/eastwood "0.1.4"]]
-        :dependencies [[slamhound "1.5.5"]]}}
+        :dependencies [[slamhound "1.5.5"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}

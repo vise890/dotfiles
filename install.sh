b6ed29e4f19002f5e8d252a0f02382f3037ea61c
Martino Visintin <mvisinti@thoughtworks.com>
2014-11-20 22:17:16 +0100

    cleanup install script

--- install.sh
+++ install.sh
@@ -3,25 +3,22 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-DOTFILES_DIR=`pwd`
-
 echo "==> copy dotfiles"
-stow --restow --target=$HOME vim zsh tmux haskell lein
+stow --restow --target="$HOME" vim zsh tmux haskell lein
 
 echo "==> install oh-my-zsh"
-rm -rf $HOME/.bash* # yeeeha!
-git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh || true
+git clone https://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" || true
 
 echo "==> install scm_breeze"
-git clone https://github.com/ndbroadbent/scm_breeze.git $HOME/.scm_breeze || true
-$HOME/.scm_breeze/install.sh
+git clone https://github.com/ndbroadbent/scm_breeze.git "$HOME/.scm_breeze" || true
+"$HOME/.scm_breeze/install.sh"
 
 echo "==> install vim-plug and plugins"
 rm -rf ~/.vim/plugged/
 mkdir -p ~/.vim/autoload
 curl -fLo ~/.vim/autoload/plug.vim \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim -u "~/.vimrc.plugs" +PlugInstall +qall!
+vim -u "$HOME/.vimrc.plugs" +PlugInstall +qall!
 
 # FIXME: massive hack to restore zshrc
 # scm_breeze should be installed manually or (better) nuked completely (since it is a dirty hack)

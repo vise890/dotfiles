12ac676237667e392e542c8f65ccd69a263dbf81
Martino Visintin <mvisinti@thoughtworks.com>
2015-01-08 16:13:20 -0500

    add whitespace

--- emacs/.emacs.d/configs/elisp.el
+++ emacs/.emacs.d/configs/elisp.el
@@ -1,7 +1,9 @@
 (require 'elisp-slime-nav)
+
 (defun my-elisp-hook ()
   (elisp-slime-nav-mode)
   (turn-on-eldoc-mode)) ;; display fun signature in minibuffer
+
 (add-hook 'emacs-lisp-mode-hook 'my-elisp-hook) ;; call my-elisp-hook when emacs-lisp-mode gets enabled
 
 (provide 'elisp)

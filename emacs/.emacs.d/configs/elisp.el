b3cc1a04910557624cc1b925cebe716bf3a6ed44
Martino Visintin <mvisinti@thoughtworks.com>
2015-01-09 16:47:11 -0500

    tidy up elisp package

--- emacs/.emacs.d/configs/elisp.el
+++ emacs/.emacs.d/configs/elisp.el
@@ -1,10 +1,18 @@
+;;; elisp.el --- Elisp config
+
+;;; Commentary:
+;; well
+
+;;; Code:
 (require 'elisp-slime-nav)
 
 (defun my-elisp-hook ()
+  "Enable all the elisp goodies."
   (elisp-slime-nav-mode)
-  (turn-on-eldoc-mode)) ;; display fun signature in minibuffer
+  (turn-on-eldoc-mode)) ; display fn signature in minibuffer
 
-(add-hook 'emacs-lisp-mode-hook 'my-elisp-hook) ;; call my-elisp-hook when emacs-lisp-mode gets enabled
+;; call my-elisp-hook when emacs-lisp-mode gets enabled
+(add-hook 'emacs-lisp-mode-hook 'my-elisp-hook)
 
 (provide 'elisp)
-
+;;; elisp.el ends here

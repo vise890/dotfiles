b9c6101c45c1a69af1a26a5311f7f36bd0291546
Martino Visintin <mvisinti@thoughtworks.com>
2015-01-09 17:00:45 -0500

    toggle projectile with C-p

--- emacs/.emacs.d/configs/vim.el
+++ emacs/.emacs.d/configs/vim.el
@@ -13,7 +13,9 @@
 
 ;; switch to normal state with C-c C-c
 (define-key evil-insert-state-map (kbd "C-c C-c") 'evil-normal-state)
+
 (define-key evil-normal-state-map (kbd "C-n") 'neotree-toggle)
+(define-key evil-normal-state-map (kbd "C-p") 'projectile-find-file)
 
 ;; esc quits everywhere
 (define-key evil-normal-state-map [escape] 'keyboard-quit)

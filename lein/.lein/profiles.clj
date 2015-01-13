b0ad3166a705e3d6ef3fd2e49566e02fb55f25ae
Martino Visintin <mvisinti@thoughtworks.com>
2015-01-13 16:14:10 -0500

    bump cider-nrepl version

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[lein-kibit "0.0.8"]
                   [jonase/eastwood "0.2.1"]
-                  [cider/cider-nrepl "0.8.2"]
+                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [lein-ancient "0.5.5"]]
         :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}

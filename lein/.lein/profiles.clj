1c89438eed0887244d555e0dc9c73636d1389fdd
Martino Visintin <mvisinti@thoughtworks.com>
2015-02-23 11:31:26 -0500

    bump lein plugin versions

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[lein-kibit "0.0.8"]
                   [jonase/eastwood "0.2.1"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [lein-ancient "0.5.5"]]
+                  [lein-ancient "0.6.3"]]
         :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}

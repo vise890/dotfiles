5da63694ba2601eff6bc32c39b64725aa4abd396
Martino Visintin <mvisinti@thoughtworks.com>
2015-02-23 11:31:38 -0500

    add cljfmt

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [[lein-kibit "0.0.8"]
                   [jonase/eastwood "0.2.1"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [lein-ancient "0.6.3"]]
+                  [lein-ancient "0.6.3"]
+                  [lein-cljfmt "0.1.10"]]
         :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}

795ab359966490eb82780d59979b79b4d1305ac1
Martino Visintin <mvisinti@thoughtworks.com>
2015-02-24 11:54:55 -0500

    enable all eastwood linters

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,7 +1,10 @@
 {:user {:plugins [[lein-kibit "0.0.8"]
-                  [jonase/eastwood "0.2.1"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [lein-ancient "0.6.3"]
-                  [lein-cljfmt "0.1.10"]]
+                  [lein-cljfmt "0.1.10"]
+
+                  [jonase/eastwood "0.2.1"]]
+        :eastwood {:linters [:all]}
+
         :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}

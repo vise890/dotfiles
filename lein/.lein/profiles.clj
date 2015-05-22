e48871cf7a12b78d07d99dcc72f60e806917c44d
Martino Visintin <martino@avaaz.org>
2015-05-22 13:17:43 +0200

    bump plugin versions

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,6 +1,6 @@
-{:user {:plugins [[lein-kibit "0.0.8"]
+{:user {:plugins [[lein-kibit "0.1.2"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [lein-ancient "0.6.3"]
+                  [lein-ancient "0.6.7"]
                   [lein-cljfmt "0.1.10"]
 
                   [jonase/eastwood "0.2.1"]]

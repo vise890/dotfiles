83c08ccc641d77b14883623111f0f00854a2158d
Martino Visintin <martino@avaaz.org>
2015-06-16 11:39:55 +0200

    use bigger fonts in zathura

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 echo "==> copy dotfiles"
-stow --restow --target="$HOME" vim zsh
+stow --restow --target="$HOME" vim zsh zathura
 
 echo "==> install oh-my-zsh"
 git clone https://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" || true

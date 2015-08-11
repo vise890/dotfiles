3dbf4894e4565e77e6bdc752621d4e05886508fd
Martino Visintin <vise890@gmail.com>
2015-08-11 11:23:52 +0200

    spartanify profiles.clj + update versions

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,13 +1,5 @@
-{:user {:plugins [[lein-kibit "0.1.2"]
-                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [refactor-nrepl  "0.3.0-SNAPSHOT"]
-                  [lein-ancient "0.6.7"]
-                  [lein-cljfmt "0.1.10"]
+{:user {:plugins [[cider/cider-nrepl "0.9.1"]
+                  [refactor-nrepl  "1.1.0"]]
 
-                  [jonase/eastwood "0.2.1"]]
-        :eastwood {:linters [:all]}
-
-        :dependencies [[slamhound "1.5.5"]
-                       [alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.7"]]
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.10"]]}}

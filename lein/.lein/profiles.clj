03cecad50d7491dbbf305070ce1011b489da7e2a
Martino Visintin <vise890@gmail.com>
2015-08-16 12:48:57 +0200

    add midje testing

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,5 +1,7 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [refactor-nrepl  "1.2.0-SNAPSHOT"]
+
+                  [lein-midje "3.1.3"]
                   [lein-cljfmt "0.3.0"]]
 
         :dependencies [[alembic "0.3.2"]

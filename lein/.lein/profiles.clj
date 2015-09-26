130033bb7b82508e8397fa7e948fcefedb9c3c2f
Martino Visintin <vise890@gmail.com>
2015-09-26 23:08:50 +0200

    add useful lein plugins

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,5 +1,9 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [refactor-nrepl  "1.2.0-SNAPSHOT"]]
+                  [refactor-nrepl  "1.2.0-SNAPSHOT"]
+
+                  [lein-cljfmt "0.3.0"]
+
+                  [lein-ancient "0.6.7"]]
 
         :dependencies [[alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.11"]]}}

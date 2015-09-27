76f83b84fb8bb01bee5a8a3645d9ac00eb421548
Martino Visintin <vise890@gmail.com>
2015-09-27 15:53:58 +0200

    moar bleeding edge, yay <_<

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [refactor-nrepl  "1.2.0-SNAPSHOT"]
+                  [refactor-nrepl  "2.0.0-SNAPSHOT"]
 
                   [lein-cljfmt "0.3.0"]
 

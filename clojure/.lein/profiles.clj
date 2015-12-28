83a26852577e82859c12c5e8c680d4b88df9cf14
Martino Visintin <vise890@gmail.com>
2015-12-28 22:16:34 +0100

    bump deps

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+{:user {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
                   [refactor-nrepl  "2.0.0-SNAPSHOT"]
 
                   [lein-cljfmt "0.3.0"]
@@ -6,4 +6,4 @@
                   [lein-ancient "0.6.7"]]
 
         :dependencies [[alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.11"]]}}
+                       [org.clojure/tools.nrepl "0.2.12"]]}}

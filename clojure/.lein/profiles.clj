05af7a7df00fa01772925c8951d11b591ebee71c
Martino Visintin <martino@mixrad.io>
2016-01-25 16:57:23 +0000

    downgrade packages to more stable versions

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,5 +1,5 @@
-{:user {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                  [refactor-nrepl  "2.0.0-SNAPSHOT"]
+{:user {:plugins [[cider/cider-nrepl "0.10.1"]
+                  [refactor-nrepl  "1.2.0"]
 
                   [lein-cljfmt "0.3.0"]
 

9ba2549d34c9e9dd7e7c6cb60cd88236dfc96002
Martino Visintin <martino@mixrad.io>
2016-01-29 17:52:43 +0000

    fiddle with profiles again

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,9 +1,11 @@
-{:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT"]
+{:user {:plugins [;[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  ;[refactor-nrepl "2.0.0-SNAPSHOT"]
+                  [refactor-nrepl "1.1.0"]
+                  [cider/cider-nrepl "0.9.1"]
 
                   [lein-cljfmt "0.3.0"]
 
                   [lein-ancient "0.6.8"]]
 
-        :dependencies [;[alembic "0.3.2"]
+        :dependencies [[alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.12"]]}}

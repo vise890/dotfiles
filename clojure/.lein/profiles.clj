ec6260263be313a3d1e1878296456079b84d1467
Martino Visintin <vise890@gmail.com>
2016-03-14 08:30:05 +0000

    add code tools. OMG tools

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -3,7 +3,13 @@
 
                   [lein-cljfmt "0.3.0"]
 
+                  [jonase/eastwood "0.2.3"]
+                  [lein-kibit "0.1.2"]
+
                   [lein-ancient "0.6.8"]]
 
-        :dependencies [[alembic "0.3.2"]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+
+        :dependencies [[slamhound "1.5.5"]
+                       [alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.12"]]}}

6aa53e1edc7eb355daa55e94040098a2d8c650ca
Martino Visintin <vise890@gmail.com>
2016-07-10 20:40:25 +0100

    reduce emacs font

--- emacs/.emacs.d/config/ui-config.el
+++ emacs/.emacs.d/config/ui-config.el
@@ -1,6 +1,6 @@
-(setq my-font "Ubuntu Mono-21")
-(set-face-attribute 'default t :font "Ubuntu Mono-21")
-(add-to-list 'default-frame-alist '(font . "Ubuntu Mono-21"))
+(setq my-font "Ubuntu Mono-16")
+(set-face-attribute 'default t :font "Ubuntu Mono-16")
+(add-to-list 'default-frame-alist '(font . "Ubuntu Mono-16"))
 
 (setq inhibit-startup-message t) ; go to scratch
 (setq initial-scratch-message "; Welcome. Welcome to city 17")

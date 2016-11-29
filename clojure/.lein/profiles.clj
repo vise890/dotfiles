8530473eefe0520af0f052dfab079462dd76447a
Martino Visintin <martino.visintin@ovoenergy.com>
2016-11-29 13:51:35 +0000

    refresh profiles.clj

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,13 +1,14 @@
-{:user {:plugins [[cider/cider-nrepl "0.14.0-SNAPSHOT"]
+{:user {:plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]
 
-                  [jonase/eastwood "0.2.3"]
-                  [lein-kibit "0.1.2"]
+                  ;[jonase/eastwood "0.2.3"]
+                  ;[lein-kibit "0.1.3"]
 
                   [lein-ancient "0.6.10"]]
 
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        ;:aliases {"slamhound" ["run" "-m" "slam.hound"]}
 
-        :dependencies [[slamhound "1.5.5"]
+        :dependencies [[org.clojure/clojure "1.8.0"]
+                       ;[slamhound "1.5.5"]
                        [alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.12"]]}}

f12bc715979e4f02610fd0a9fcff282921820e87
Martino Visintin <martino.visintin@ovoenergy.com>
2017-01-06 12:02:03 +0000

    clj stuff

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,14 +1,19 @@
 {:user {:plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]
 
-                  ;[jonase/eastwood "0.2.3"]
-                  ;[lein-kibit "0.1.3"]
+                  [jonase/eastwood "0.2.3"]
+                  [lein-kibit "0.1.3"]
+                  [lein-cljfmt "0.5.6"] ; lein cljfmt fix
+                  [lein-bikeshed "0.4.1"]
 
+                  ;[lein-ns-dep-graph "0.2.0-SNAPSHOT"]
+
+                  ;; NOTE: lein ancient upgrade-profiles to upgrade this file
                   [lein-ancient "0.6.10"]]
 
-        ;:aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
 
         :dependencies [[org.clojure/clojure "1.8.0"]
-                       ;[slamhound "1.5.5"]
+                       [slamhound "1.5.5"]
                        [alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.12"]]}}

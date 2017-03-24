5ee84d75e963220293fcbe3f39cfdb4843bf97ae
Martino Visintin <martino.visintin@ovoenergy.com>
2017-03-24 15:54:02 +0000

    replace humane-test-output and clj-stacktrace with ultra

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -14,6 +14,10 @@
                   ;; NOTE: lein ancient upgrade-profiles to upgrade this file
                   [lein-ancient "0.6.10"]]
 
+        ;; :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+
         :dependencies [[org.clojure/clojure "1.8.0"]
+                       ;; [slamhound "1.5.5"]
+
                        [alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.12"] ]}}
+                       [org.clojure/tools.nrepl "0.2.12"]]}}

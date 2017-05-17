bc3556280e01b3763aa0ab7ced91ec751881c2bb
Martino Visintin <vise890@gmail.com>
2017-05-17 21:27:11 +0100

    upgrades

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,11 +1,11 @@
 {:user {:plugins [[org.clojure/clojure "1.8.0"]
                   [cider/cider-nrepl "0.15.0-SNAPSHOT"]
-                  [refactor-nrepl "2.3.0-SNAPSHOT"]
+                  [refactor-nrepl "2.3.0"]
 
                   [venantius/ultra "0.5.1"]
 
                   [jonase/eastwood "0.2.3"]
-                  [lein-kibit "0.1.3"]
+                  [lein-kibit "0.1.5"]
 
                   [lein-bikeshed "0.4.1"]
                   [lein-cljfmt "0.5.6"] ; lein cljfmt fix
@@ -21,4 +21,4 @@
                        ;; [slamhound "1.5.5"]
 
                        [alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.12"]]}}
+                       [org.clojure/tools.nrepl "0.2.13"]]}}

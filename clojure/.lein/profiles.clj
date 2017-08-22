8403e8ab3c82f3597894129922e891df518cb2c3
Martino Visintin <martino.visintin@ovoenergy.com>
2017-08-22 13:54:30 +0100

    disable everything

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -7,20 +7,17 @@
 
                   [lein-hiera "0.9.5"]
 
-                  [jonase/eastwood "0.2.4"]
-                  [lein-kibit "0.1.5"]
-                  [lein-bikeshed "0.4.1"]
+                  ;; [jonase/eastwood "0.2.4"]
+                  ;; [lein-kibit "0.1.5"]
+                  ;; [lein-bikeshed "0.4.1"]
 
-                  [lein-cljfmt "0.5.7"] ; lein cljfmt fix
+                  ;; [lein-cljfmt "0.5.7"] ; lein cljfmt fix
 
                   ;; NOTE: lein ancient upgrade-profiles to upgrade this file
-                  [lein-ancient "0.6.10"]
+                  ;; [lein-ancient "0.6.10"]
 
-                  [lein-marginalia "0.9.0"] ]
+                  ;; [lein-marginalia "0.9.0"]
+                  ]
 
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
-
-        :dependencies [[slamhound "1.5.5"]
-
-                       [alembic "0.3.2"]
+        :dependencies [[alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.13"]] }}

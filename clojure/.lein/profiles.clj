8420a7e7dfe386289e5566645fd2f8d8c78ebe03
Martino Visintin <vise890@gmail.com>
2017-09-18 19:13:27 +0200

    rename profiles

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -7,17 +7,18 @@
 
  :misc {:plugins [[lein-marginalia "0.9.0"]
                   [lein-hiera "0.9.5"]
-                  [org.clojure/tools.analyzer "0.6.9"]]}
+                  [lein-cljfmt "0.5.7"] ;; lein cljfmt fix
+                  ;; NOTE: lein ancient upgrade-profiles to upgrade this file
+                  [lein-ancient "0.6.10"]]}
 
- :bikeshedding {:plugins [[jonase/eastwood "0.2.4"]
-                          [lein-kibit "0.1.5"]
-                          ;; NOTE: lein ancient upgrade-profiles to upgrade this file
-                          [lein-ancient "0.6.10"]
-                          [lein-cljfmt "0.5.7"] ;; lein cljfmt fix
-                          [lein-bikeshed "0.4.1"]]
+ :lint {:plugins [
+                  [jonase/eastwood "0.2.4"]
+                  [lein-kibit "0.1.5"]
+                  [lein-bikeshed "0.4.1"]
+                  [venantius/yagni "0.1.4"]]
 
-                :dependencies [[slamhound "1.5.5"]]
-                :aliases      {"slamhound" ["run" "-m" "slam.hound"]}}
+        :dependencies [[slamhound "1.5.5"]]
+        :aliases      {"slamhound" ["run" "-m" "slam.hound"]}}
 
  :fancy-testing {:plugins [[venantius/ultra "0.5.1"]
                            [lein-eftest "0.3.1"]
